45716939b6685d9db5b36f034e75d9fc592fee30
meow-d <isaacwenyi@gmail.com>
2024-02-28 17:56:44 +0800

    fix broken IDKnull response because it return a tuple now

--- application.py
+++ application.py
@@ -41,7 +41,7 @@ def get_bot_response():
         userText = ""
     botReply, movie_id = getResponse(userText)
     if botReply == "IDKresponse":
-        botReply = str(getResponse('IDKnull')) ##Send the "i don't know" code back to the DB
+        botReply = str(getResponse('IDKnull')[0]) ##Send the "i don't know" code back to the DB
         if useGoogle == "yes":
             botReply = botReply + tryGoogle(userText)
     elif botReply == "getTIME":
