8dcbc742d13e76b946ce07bb57185b90cfc98553
meow-d <isaacwenyi@gmail.com>
2024-02-29 18:07:11 +0800

    switch to spaCy for matching instead of just difflib proper nlp, baby! also optimized the code by loading the csv at the beginning, instead of every single time. what were they thinking and yes, the csv is processed at the beginning too.

--- botRespond.py
+++ botRespond.py
@@ -3,10 +3,26 @@ from difflib import SequenceMatcher
 import urllib.parse
 import csv
 import random
-from recommendationEngine import movie_menu
+import spacy
 
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
+# TODO where should i even put this code
+print("Loading dataset...")
+with open('data/chatbot.csv') as g:
+    lines = list(csv.reader(g))
+    lineCount = 0
+    for i in reversed(range(len(lines))):
+        if not (lines[i][0] and lines[i][1]):
+            print(f"WARNING: {lines[i]} skipped due to missing data")
+            del lines[i]
+    data = lines[2:] # list slicing to filter out headings
+
+# if you don't have it, install spacy and run `python -m spacy download en_core_web_md`
+print("Loading spaCy model...")
+nlp = spacy.load("en_core_web_md")
+
+print("Loading dataset into spaCy...")
+data_humansays_only = [line[0] for line in data]
+data_doc = list(nlp.pipe(data_humansays_only))
 
 def getResponse(sendMsg: str) -> tuple[str, int|None]:
     #return "You said: " + sendMsg
@@ -16,34 +32,32 @@ def getResponse(sendMsg: str) -> tuple[str, int|None]:
         movie_id = int(sendMsg)
         return "MOVIEmenu", movie_id
 
-    #Loop through CSV knowledge file.  If a question is equal to or greater than the confidence level, add it to a list of possible responses. Then return a random responses
-    lineCount = 0
+    # TODO check for any direct commands
+    # TODO keywords
+
     successCount = 0
     exactCount = 0
     comeBacks = []
     exactReply = []
     exactMatch = .9
-    with open('data/chatbot.csv') as g:
-        lines = csv.reader(g)
-        for line in lines:
-            lineCount += 1
-            if not line[0] or not line[1]:
-                emptyCount += 1
-                print("WARNING: I had to skip row #" + str(lineCount) + " due to missing data.")
-            if lineCount > 1 and line[0] and line[1]:
-                userText = line[0]
-                botReply = line[1]
-                checkMatch = similar(sendMsg, userText)
-                if checkMatch >= exactMatch:
-                    exactCount += 1
-                    exactReply.append(botReply)
-                    print("Likely match: " + userText)
-                    print("Match is: " + str(checkMatch))
-                elif checkMatch >= confidenceLevel:
-                    successCount += 1
-                    comeBacks.append(botReply)
-                    print("Possible match: " + userText)
-                    print("Match is: " + str(checkMatch))
+    sendMsg_doc = nlp(sendMsg)
+
+    print("Checking for matches...")
+    for i, line_doc in enumerate(data_doc):
+        userText = data[i][0]
+        botReply = data[i][1]
+        checkMatch = sendMsg_doc.similarity(line_doc)
+        print(f"Match for {userText}: {checkMatch}")
+        if checkMatch >= exactMatch:
+            exactCount += 1
+            exactReply.append(botReply)
+            print("Likely match: " + userText)
+            print("Match is: " + str(checkMatch))
+        elif checkMatch >= confidenceLevel:
+            successCount += 1
+            comeBacks.append(botReply)
+            print("Possible match: " + userText)
+            print("Match is: " + str(checkMatch))
     if exactCount >= 1:
         botResponsePick = random.choice(exactReply)
     elif successCount >= 1:
