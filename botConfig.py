f91778500c45c7850ab852e271a8bae379bd9a2f
meow-d <isaacwenyi@gmail.com>
2024-03-08 23:02:11 +0800

    Adjust confidence level

--- botConfig.py
+++ botConfig.py
@@ -6,7 +6,7 @@ botTimeZone = "Asia/Kuala_Lumpur"  ##See the list full below
 botAvatar = "/static/Ayaka.png" ##This can either be an image in your static folder or a web url to an image
 chatBG = "/static/bg.jpg"  ##This can either be an image in your static folder or a web url to an image
 useGoogle = "yes" ## yes or no if you want Google links provided when your Bot does not have an answer
-confidenceLevel = 0.80 ##Bot confidence level - must be betwee. 0.00 and 1, default is 0.65
+confidenceLevel = 0.75 ##Bot confidence level - must be betwee. 0.00 and 1, default is 0.65
 DATASET_PATH = "data/ml-latest-small/"
 SEARCH_WORDS = {
     "recommendation",
